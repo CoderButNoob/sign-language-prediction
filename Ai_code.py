dd06db251f88de3b45888d3f22669466ac3b9132
CoderButNoob <131383477+CoderButNoob@users.noreply.github.com>
2024-05-05 22:08:50 +0530

    Add files via upload

---
+++ Ai_code.py
@@ -0,0 +1,89 @@
+#importing libraries import numpy as np import pandas as pd import random as rd
+#data visualization
+import matplotlib.pyplot as plt
+%matplotlib inline import seaborn as sns
+import plotly.express as px from PIL import Image
+#for the CNN model
+from sklearn.model_selection import train_test_split from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing from keras.preprocessing.image import ImageDataGenerator
+#setting seed for reproducability from numpy.random import seed seed(10)
+tf.random.set_seed(20)
+#for viewing filenames import os
+for dirname, _, filenames in os.walk('./input'): for filename in filenames:
+print(os.path.join(dirname, filename))
+#downloading the training data
+train = pd.read_csv("./input/sign-language-mnist/sign_mnist_train.csv") train.head()
+#downloading the test data
+test = pd.read_csv("./input/sign-language-mnist/sign_mnist_test.csv") test.head()
+#summing the number of na in the training set for each column print(sum(train.isna().sum()))
+#summing the number of na in the test set for each column print(sum(test.isna().sum()))
+#summing the number of null values in the training set for each column print(sum(train.isnull().sum()))
+#summing the number of null values in the test set for each column print(sum(test.isnull().sum()))
+#creating our Y for the training data
+Y_train = train["label"]
+#creating our X for the training data
+X_train = train.drop(labels = ["label"],axis = 1)
+#creating our Y for the test data Y_test = test["label"]
+#creating our X for the training data
+X_test = test.drop(labels = ["label"],axis = 1)
+#converting the range of the pixel data from 0-255 to 0-1 X_train = X_train / 255.0
+X_test = X_test / 255.0
+X_train = X_train.values.reshape(-1,28,28,1) X_test = X_test.values.reshape(-1,28,28,1) print(X_train.shape)
+print(X_test.shape)
+#creating an interactive bar graph that shows the distrubition of labels within the training set
+fig = px.histogram(train,
+x='label',
+color = 'label',
+title="Distrubition of Labels in the Training Set", width=700, height=500)
+fig.show()
+#creating an interactive bar graph that shows the distrubition of labels within the test set
+fig = px.histogram(test,
+x='label',
+color = 'label',
+title="Distrubition of Labels in the Test Set", width=700, height=500)
+fig.show()
+#creating a 5x5 grid of the first 25 photos in the training images fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(15, 10),
+subplot_kw={'xticks': [], 'yticks': []})
+for i in range(25): plt.subplot(5,5,i+1) plt.imshow(X_train[i], cmap='gray') plt.title(Y_train[i])
+plt.show()
+#creating a 5x5 grid of the first 25 photos in the test images fig, axes = plt.subplots(nrows=5, ncols=5, figsize=(15, 10),
+subplot_kw={'xticks': [], 'yticks': []})
+for i in range(25): plt.subplot(5,5,i+1) plt.imshow(X_test[i], cmap='gray') plt.title(Y_test[i])
+plt.show()
+#spliting training images into the images we will use for training the model and validating the model
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.3, random_state=7)
+#showingthe shapes of our train, validate, and test images print(X_train.shape)
+print(Y_train.shape) print(X_val.shape) print(Y_val.shape) print(X_test.shape) print(Y_test.shape)
+#creating our CNN model model = keras.Sequential([
+layers.BatchNormalization(),
+layers.Conv2D(filters=32, kernel_size=(5,5), activation="relu", padding='same',
+input_shape=[28, 28, 1]), layers.MaxPool2D(), layers.Dropout(.25),
+layers.BatchNormalization(),
+layers.Conv2D(filters=32, kernel_size=(3,3), activation="relu", padding='same'),
+layers.MaxPool2D(), layers.Dropout(.25),
+layers.BatchNormalization(),
+layers.Conv2D(filters=64, kernel_size=(3,3), activation="relu", padding='same'),
+layers.MaxPool2D(), layers.Dropout(.25),
+layers.BatchNormalization(),
+layers.Conv2D(filters=128, kernel_size=(3,3), activation="relu", padding='same'),
+layers.MaxPool2D(), layers.Dropout(.25),
+layers.Flatten(), layers.Dropout(.25)
+layers.Dense(units=64, activation="relu"), layers.Dense(units=26, activation="softmax"),
+])
+#compiling the model model.compile(
+optimizer=tf.keras.optimizers.Adam(epsilon=0.01), loss='sparse_categorical_crossentropy', metrics=['accuracy']
+)
+#Training the model history = model.fit(
+x = X_train, y = Y_train,
+validation_data= (X_val,Y_val), batch_size = 128,
+epochs=50, verbose=2,
+)
+#Viewing the training results
+history_frame = pd.DataFrame(history.history) history_frame.loc[:, ['loss', 'val_loss']].plot() history_frame.loc[:, ['accuracy', 'val_accuracy']].plot();
+#creating our predictions using the test pixel values predictions = model.predict(X_test)
+predictions = np.argmax(predictions,axis = 1)
+12
+#creating a report that show how our predictions compare with actual values print(classification_report(Y_test, predictions))
